34239e1dfcbd2f37dc07d52d0750fed11ade3f91
Folcon <folcon@gmail.com>
2022-03-09 01:05:45 +0000

    Tweak `radio-button` to not paint when inactive
    Stops painting the filled in circle when not active.

--- src/fe_7drl_2022/components.clj
+++ src/fe_7drl_2022/components.clj
@@ -4,23 +4,24 @@
   (:import [io.github.humbleui.skija Canvas Paint PaintMode]))
 
 
-(defn draw-radio-toggle-impl [fill scale]
+(defn draw-radio-toggle-impl [active? fill scale]
   (fn [^Canvas canvas _window-width _window-height]
     (let [border (doto (Paint.)
                    (.setColor (unchecked-int 0xFF000000))
                    (.setMode PaintMode/STROKE)
                    (.setStrokeWidth (* 1 scale)))]
       (.drawCircle canvas 5 5 10 border)
-      (.drawCircle canvas 5 5 6 fill))))
+      (if active?
+        (.drawCircle canvas 5 5 6 fill)))))
 
 (defn radio-button [state value selected-path child]
   (ui/clickable
     #(swap! state assoc-in selected-path value)
-    (ui/dynamic ctx [{:keys [fill-green fill-black scale]} ctx
+    (ui/dynamic ctx [{:keys [fill-black scale]} ctx
                      selected (get-in @state selected-path)]
       (ui/column
         (ui/row
           (c-hui/custom-ui 20 20
-            {:on-paint (#'draw-radio-toggle-impl (if (= selected value) fill-black fill-green) scale)}))
+            {:on-paint (#'draw-radio-toggle-impl (= selected value) fill-black scale)}))
         (ui/gap 5 0)
         child))))
