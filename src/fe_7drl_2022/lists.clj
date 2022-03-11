9ead32a755466cb7726b71548c649a03a9890391
Folcon <folcon@gmail.com>
2022-03-11 20:33:30 +0000

    Add adjectives list for flavour

---
+++ src/fe_7drl_2022/lists.clj
@@ -0,0 +1,22 @@
+(ns fe-7drl-2022.lists)
+
+
+(def adjectives
+  {:adorable "🥺"
+   :aggressive "😡"
+   :agreeable "😃"
+   :angry "😠"
+   :average "😐"
+   :brainy "🧠"
+   :cheerful "😀"
+   :clever "🤓"
+   :confused "😕"
+   :delightful "😄"
+   :elegant "🧐"
+   :expensive "😘"
+   :frightened "😱"
+   :tired "😫"
+   :rich "🤑"
+   :weary "😩"
+   :worried "😟"
+   :zany "🤪"})
